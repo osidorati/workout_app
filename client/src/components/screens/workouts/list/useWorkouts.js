b0757e70d3586207030facedaa6481e83e3650ed
osidorati <loksana864@gmail.com>
2023-03-11 18:48:01 +0300

    feat: add workout item (check bag)

--- client/src/components/screens/workouts/list/useWorkouts.js
+++ client/src/components/screens/workouts/list/useWorkouts.js
@@ -1,3 +1,34 @@
-export const useWorkouts = () => {
+import { useMutation, useQuery } from '@tanstack/react-query'
+import WorkoutService from '../../../../services/workout/workout.service.js'
+import WorkoutLogService from '../../../../services/workout/workout-log.service.js'
+import { useNavigate } from 'react-router-dom'
 
+export const useWorkouts = () => {
+	const { data, isSuccess } = useQuery(['get workouts'],
+		() => WorkoutService.getAll(),
+		{
+			select: ({ data }) => data
+		})
+
+	const navigate = useNavigate()
+
+	const {
+		mutate: mutate,
+		isLoading,
+		isSuccess: isSuccessMutate,
+		error
+	} = useMutation(['Create new workout log'],
+		(workoutId) => WorkoutLogService.create(),
+		{onSuccess(data) {
+			navigate(`/workout/${data.id}`)}
+		})
+
+	return {
+		data,
+		isSuccess,
+		mutate,
+		isLoading,
+		isSuccessMutate,
+		error
+	}
 }
