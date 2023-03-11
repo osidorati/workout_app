22277bb77c8ccdb6edeb15b41d730ac085a182da
osidorati <loksana864@gmail.com>
2023-03-12 01:51:20 +0300

    fix: add open auth page without auth

--- client/src/components/screens/not-found/NotFound.jsx
+++ client/src/components/screens/not-found/NotFound.jsx
@@ -1,6 +1,18 @@
 import Layout from '../../layout/Layout.jsx'
+import { useAuth } from '../../../hooks/useAuth.js'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const NotFound = () => {
+	const {isAuth} = useAuth()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (!isAuth) {
+			navigate('/auth')
+		}
+	})
+
 	return (
 		<>
 			<Layout heading='Page not found'/>
